b09840af09d2075ef38bc18edccfde641f2d2d83
IsThisFun <rodricarlosroj@gmail.com>
2017-06-05 21:51:29 -0500

    Update guess.rb

--- guess.rb
+++ guess.rb
@@ -3,32 +3,35 @@ puts
 puts "What's your name?"
 name = gets.chomp.to_i
 puts "Pick your max range"
+puts
 large_num = gets.chomp.to_i
 puts "How many tries would you like?"
+puts
 num_tries = gets.chomp.to_i
 ran_num = rand(large_num) + 1
 guess = 0
 tries = 0
-puts "Go!!!"
+puts
+print "Go!!!"
 until guess == ran_num || tries == large_num
 puts
 puts"----------------------------------------"
-puts "Guess a number between 1 and #{large_num}"
+print "Guess a number between 1 and #{large_num}"
 guess = gets.chomp.to_i
 if guess > ran_num 
   puts "------- Too High. Guess Lower --------"
 elsif guess < ran_num
   puts "------- Too Low. Guess Higher --------"  
 elsif guess == ran_num
-puts "WOAH YOU'RE A WIZARD {#name}"
+print "WOAH YOU'RE A WIZARD {#name}"
 end
 tries += 1
 limit = num_tries - tries 
-puts "You have #{limit} out of #{num_tries}"
+print "You have #{limit} out of #{num_tries}"
 if tries == num_tries 
-  puts "GAME OVER"
+  print "GAME OVER"
   10000000.times do
-  puts ":("
+  print ":("
   end
   end
   end
